6258c5620053bc1f1cc91e5414557341c283328c
ajchemist <1694505+aJchemist@users.noreply.github.com>
2020-10-25 12:49:14 +0900

    Setup `cider-clojure-cli-global-options' in .dir-locals.el

--- library-for-github/.dir-locals.el
+++ library-for-github/.dir-locals.el
@@ -1,2 +1,3 @@
 ((clojure-mode
-  (cider-clojure-cli-global-options . "-A:test")))
+  ;; (cider-clojure-cli-global-options . "-A:test")
+  (cider-clojure-cli-parameters . "-M:test -m nrepl.cmdline --middleware '%s'")))
